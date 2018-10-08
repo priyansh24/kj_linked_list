3425aab7774770453c03645349d7d9fb453d7a9c
joshi98kishan <43925446+joshi98kishan@users.noreply.github.com>
2018-10-08 12:09:43 +0530

    Added create function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
+#include<iostream.h>
 using namespace std;
 
 struct node{
   int data;
   struct node* next;
-}start*=NULL;
+}*start=NULL;
 
 typedef struct node Node;
 
 void main(){
   
+}
+
+Node* createNode(int data){
+  Node* ptr;
+  ptr=(Node*) malloc(sizeof(Node));
+  ptr->data=data;
+  ptr->next=NULL;
+  
+  return ptr;
 }
